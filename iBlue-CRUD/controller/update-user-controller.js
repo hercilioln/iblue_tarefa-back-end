c5bddd6f5c3823f4b4c9fa02ba177a94bb352a40
Hercilio <contato@hercilio.me>
2022-05-14 00:04:04 -0300

    Correção no Controller

--- iBlue-CRUD/controller/update-user-controller.js
+++ iBlue-CRUD/controller/update-user-controller.js
@@ -1,18 +1,8 @@
 const Joi = require('joi');
 
 const updateUserSchema = Joi.object().keys({
-  nome: Joi.string().min(5).max(30).required().messages({
-    'string.base': `"nome" should be a type of 'text'`,
-    'string.empty': `"nome" cannot be an empty field`,
-    'string.min': `"nome" should have a minimum length of {#limit}`,
-    'any.required': `"nome" is a required field`
-  }), 
-  dataNascimento: Joi.date().required().messages({
-    'string.base': `"dataNascimento" should be a type of 'text'`,
-    'string.empty': `"dataNascimento" cannot be an empty field`,
-    'string.min': `"dataNascimento" should have a minimum length of {#limit}`,
-    'any.required': `"dataNascimento" is a required field`
-  }),
+  nome: Joi.string().min(5).max(30).required(), 
+  dataNascimento: Joi.date().required(),
 })
 
 const validacaoUpadateUser = (request, response) => {
